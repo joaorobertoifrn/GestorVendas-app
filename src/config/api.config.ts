a0b50dc9830b65b49129d0d7254040bb0c4af7e8
João Roberto <joao_roberto@msn.com>
2018-03-04 21:16:29 -0300

    Mostrando as Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: 'https://gestorvendas.herokuapp.com/'
+    baseUrl: 'https://gestorvendas.herokuapp.com/',
+    bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/helpc-developer'
+
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -8,6 +10,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
 
   constructor(
     public navCtrl: NavController, 
@@ -18,7 +24,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
     error => {
       console.log(error);

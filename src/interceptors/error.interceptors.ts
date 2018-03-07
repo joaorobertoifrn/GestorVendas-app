ca28bd5effe21ccd5c681983c7ea6c36b78616c8
João Roberto <joao_roberto@msn.com>
2018-03-06 22:27:18 -0300

    Salvando novo cliente

--- src/interceptors/error.interceptors.ts
+++ src/interceptors/error.interceptors.ts
@@ -3,6 +3,8 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldsmessage';
+
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -38,6 +40,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 404:
                     this.handle404();
                     break;
+                
+                case 422:
+                    this.handle422(errorObj);
+                    break;
+                        
                 default:
                     this.handleDefaultEror(errorObj);
             }
@@ -69,7 +76,21 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
-        
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+    
+    
     handleDefaultEror(errorObj) {
         let alert = this.alertCtrl.create({
             title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
@@ -79,6 +100,15 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
             alert.present();        
     }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
+    }
+        
 }
 
 export const ErrorInterceptorProvider = {

77f1fd094c4b1058ac505b8fbd1e7baa3a8f91ab
João Roberto <joao_roberto@msn.com>
2018-03-06 21:58:25 -0300

    Mostrando erros de validação

--- src/services/cidade.service.ts
+++ src/services/cidade.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { API_CONFIG } from "../../config/api.config";
-import { CidadeDTO } from "../../models/cidade.dto";
 import { Observable } from "rxjs/Rx";
+import { CidadeDTO } from "../models/cidade.dto";
+import { API_CONFIG } from "../config/api.config";
 
 @Injectable()
 export class CidadeService {

c4ee10925a0b0780f6ef1312e0d93d7460254ab2
João Roberto <joao_roberto@msn.com>
2018-03-07 22:40:42 -0300

    Carregando produtos de uma dada categoria

--- src/services/domain/estado.service.ts
+++ src/services/domain/estado.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { API_CONFIG } from "../config/api.config";
-import { EstadoDTO } from "../models/estado.dto";
+import { API_CONFIG } from "../../config/api.config";
+import { EstadoDTO } from "../../models/estado.dto";
 import { Observable } from "rxjs/Rx";
 
 @Injectable()

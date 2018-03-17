9d22ddf84c5f9d3f152d9225e549403a1eaafbbf
João Roberto <joao_roberto@msn.com>
2018-03-17 18:19:27 -0300

    projeto final

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
     baseUrl: 'https://gestorvendas.herokuapp.com/',
+    //baseUrl: 'http://localhost:8080/',
     bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/helpc-developer'
 
 }
